1e1bba57eff4811b2ad8a77fdf4eca23bea5dc45
Audrius Molis <audrius.molis@quuppa.com>
2021-09-19 15:54:27 +0300

    Draw links first

--- src/burbulai/drawer.clj
+++ src/burbulai/drawer.clj
@@ -3,11 +3,11 @@
 
 (defn draw [state]
   (q/background 100)
-  (doseq [{:keys [color x y]} (vals (:particles state))]
-    (q/fill color 255 255)
-    (q/ellipse x y 8 8))
   (doseq [[start-particle-id end-particle-id] (:links state)]
     (q/fill 255 255 255)
     (let [start-particle (start-particle-id (:particles state))
           end-particle (end-particle-id (:particles state))]
-      (q/line (:x start-particle) (:y start-particle) (:x end-particle) (:y end-particle)))))
+      (q/line (:x start-particle) (:y start-particle) (:x end-particle) (:y end-particle))))
+  (doseq [{:keys [color x y]} (vals (:particles state))]
+    (q/fill color 255 255)
+    (q/ellipse x y 8 8)))
