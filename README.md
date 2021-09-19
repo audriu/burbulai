8bad894a5540b46c9cfd7fb39539abdc492492f3
Audrius Molis <audrius.molis@quuppa.com>
2021-09-19 20:55:38 +0300

    Some formatting

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Burbulai
 
 A Quil sketch for particle sim.
+
+
+## Inspired by
+https://www.youtube.com/watch?v=3HjO_RGIjCU
+and
+https://040code.github.io/2018/03/04/verlet-integration
--- src/burbulai/constants.clj
+++ src/burbulai/constants.clj
@@ -3,9 +3,9 @@
 (def world-width 700)
 (def world-height 700)
 
-(def initial-state {:particles
-                           {:p0 {:x 50 :y 660 :dx 2 :dy 8 :color 40}
-                            :p1 {:x 150 :y 60 :dx 2 :dy 1 :color 140}
-                            :p2 {:x 90 :y 360 :dx -2 :dy -1 :color 240}}
-                    :links [[:p0 :p1 300]
-                            [:p1 :p2 200]]})
+(def initial-state {:particles {:p0 {:x 50 :y 660 :dx 2 :dy 8 :color 40}
+                                :p1 {:x 150 :y 60 :dx 2 :dy 1 :color 140}
+                                :p2 {:x 90 :y 360 :dx -2 :dy -1 :color 240}}
+                    :links     [[:p0 :p1 300]
+                                [:p0 :p2 200]
+                                [:p1 :p2 200]]})
